0308e9b45a9a0e7fc0f97aebd98401f7e75b709e
John Paulo Barbin <john.barbin@boom.camp>
2019-05-23 13:25:26 +0800

    Problem 4

--- practice.js
+++ practice.js
@@ -24,6 +24,7 @@ var lovesCode = true || false;
 // Create a variable called greatestFear that is undefined because we fear nothing
 
 // Code here
+var greatestFear;
 
 //////////////////PROBLEM 5////////////////////
 
