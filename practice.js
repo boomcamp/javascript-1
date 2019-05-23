ded8fd7e74c312b28455f228c58dcf92d7aecedd
armanbayona <arman.bayona@boom.camp>
2019-05-23 14:09:48 +0800

    solve 11-14

--- practice.js
+++ practice.js
@@ -113,11 +113,15 @@ function nameCheck(name){
 // The add function should return the two parameters added together
 
 // Code Here
+function add(a, b){
+  return a + b;
+}
 
 // Now invoke add, passing in the numbers 3 and 4
 // storing the result in the variable mathSum.
 
 // Code Here
+var mathSum = add(3, 4);
 
 //////////////////PROBLEM 12////////////////////
 
@@ -129,6 +133,21 @@ function nameCheck(name){
 // Otherwise, you should return the string 'you need to evaluate your favorite color choice'
 
 // Code here
+function faveColorFinder(color){
+  if(color == "red"){
+    return 'red is a great color'
+  }
+  else if(color == "green"){
+    return 'green is a solid favorite color'
+  }
+    
+  else if(color == "black"){
+    return 'so trendy'
+  }  
+  else{
+    return 'you need to evaluate your favorite color choice'
+  }
+}
 
 //////////////////PROBLEM 13////////////////////
 
@@ -152,28 +171,29 @@ function pond() {
 // as strings.
 
 // This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ['duck', 'sailorDuck', 'rubberDuck', 'realDuck'];
+let globalScope = ['duck'];
 
 // This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ['duck', 'sailorDuck', 'rubberDuck', 'realDuck'];
+let bathroomScope = ['duck', 'rubberDuck'];
 
 // This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ['duck', 'sailorDuck', 'rubberDuck', 'realDuck'];
+let bathtubScope = ['duck', 'sailorDuck', 'rubberDuck'];
 
 // This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ['duck', 'sailorDuck', 'rubberDuck', 'realDuck'];
+let pondScope = ['duck', 'realDuck'];
 
 //////////////////PROBLEM 14////////////////////
 
 // Create a variable called age with your age assigned to you
 
 // Code Here
-
+let age = 20;
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
-
+age + 1
 // Code Here
 
 // Good news! We can live forever. Set your age to 999
 
 // Code Here
+age = 999
