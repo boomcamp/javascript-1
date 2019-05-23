2529f05cf8e1cd00a42184b7bcec1ec5a1a14b3a
Martin Raquion <raquionearl@gmail.com>
2019-05-23 13:00:33 +0800

    Solved Problem 11

--- practice.js
+++ practice.js
@@ -115,15 +115,15 @@ function nameCheck(name){
 // The add function should return the two parameters added together
 
 // Code Here
-// function add(num1,num2){
-//   return num1+num2;
-// }
+function add(num1,num2){
+  return num1+num2;
+}
 
 // // Now invoke add, passing in the numbers 3 and 4
 // // storing the result in the variable mathSum.
 
 // // Code Here
-// mathSum = add(3,4);
+mathSum = add(3,4);
 
 //////////////////PROBLEM 12////////////////////
 
