3a10328c397d9cd6942d8e6bef573e4251cb0d76
Jules <jules.ballaran@boom.camp>
2019-05-23 13:01:29 +0800

    Solved PROBLEM 10

--- practice.js
+++ practice.js
@@ -90,6 +90,17 @@ var boomCampClassPet = dog.name;
 // with NAMEPARAM being the name parameter being passed in
 
 // Code here
+function nameCheck(name) {
+  if(name === 'Steven'){
+    return 'What is up Steven?';
+  }
+  else if (name === 'Bryan'){
+    return 'Hey Bryan!';
+  }
+  else{
+    return 'Cool name, '+ name;
+  }
+}
 
 //////////////////PROBLEM 11////////////////////
 
