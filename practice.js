12a6f50b18ab1a63dee80880f9c6587adde87bfd
Samuel A. Lopez <samuel.lopez@boom.camp>
2019-05-23 13:14:48 +0800

    Problem 12 Done

--- practice.js
+++ practice.js
@@ -129,7 +129,20 @@
 // Otherwise, you should return the string 'you need to evaluate your favorite color choice'
 
 // Code here
-
+        function faveColorFinder(color){
+          if(color === 'red'){
+            return 'red is a great color';
+          }
+          else if(color === 'green'){
+            return 'green is a solid favorite color';
+          }
+          else if(color === 'black'){
+            return 'so trendy';
+          }
+          else{
+            return 'you need to evaluate your favorite color choice';
+          }
+        }
 //////////////////PROBLEM 13////////////////////
 
 let duck = 'cute';
