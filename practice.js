966a73a0bbea7479a502aef4a15dca0172c6f688
Jan Patrick L. Bañares <janpatrick.banares@boom.camp>
2019-05-23 13:32:08 +0800

    completed problem 13

--- practice.js
+++ practice.js
@@ -152,16 +152,16 @@ function pond() {
 // as strings.
 
 // This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ['duck', 'sailorDuck', 'rubberDuck', 'realDuck'];
+let globalScope = ["duck"];
 
 // This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ['duck', 'sailorDuck', 'rubberDuck', 'realDuck'];
+let bathroomScope = ["rubberDuck","duck"];
 
 // This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ['duck', 'sailorDuck', 'rubberDuck', 'realDuck'];
+let bathtubScope = ["sailorDuck","duck","rubberDuck"];
 
 // This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ['duck', 'sailorDuck', 'rubberDuck', 'realDuck'];
+let pondScope = ["realDuck","duck"];
 
 //////////////////PROBLEM 14////////////////////
 
